1a70c98cf634862e83d2944acb6cfd8fb82f7a3f
Mikhail Rybolovlev <michaels17525@mail.ru>
2020-03-02 14:17:07 +0300

    Solve the first problem. Switch DEF_WIDTH and DEF_HEIGHT variables in graph.py to 1600 and 900. Function windowSize() isn't working.

---
+++ Task #2.py
@@ -0,0 +1,25 @@
+from graph import *
+
+
+def ufo(x, y, size):
+    pass
+
+
+def alien(x, y, size, invert):
+    pass
+
+
+def sky():
+    pass
+
+
+width = 1600
+height = 900
+windowSize(width, height)
+penColor(0, 0, 0)
+brushColor(25, 25, 112)
+print(coords(rectangle(0, 0, width, height / 3 * 2)))
+penColor(0, 0, 0)
+brushColor(0, 100, 0)
+rectangle(0, height / 3 * 2, width, height)
+run()
