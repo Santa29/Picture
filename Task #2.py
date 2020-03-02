15a4c05b6d970cac79566083e3b534d01fe8ac03
Mikhail Rybolovlev <michaels17525@mail.ru>
2020-03-02 15:49:31 +0300

    Change the task file cause the library is broken.

--- Task #2.py
+++ Task #2.py
@@ -1,15 +1,15 @@
 from graph import *
 
 
-def ufo(x, y, size):
+def hedgehog(x, y, size):
     pass
 
 
-def alien(x, y, size, invert):
+def light(x, y, size, invert):
     pass
 
 
-def sky():
+def mushroom(angle, size):
     pass
 
 
@@ -17,11 +17,9 @@ width = 1600
 height = 900
 windowSize(width, height)
 penColor(0, 0, 0)
-brushColor(25, 25, 112)
+brushColor(0, 150, 0)
 print(coords(rectangle(0, 0, width, height / 3 * 2)))
 penColor(0, 0, 0)
-brushColor(0, 100, 0)
+brushColor('grey')
 rectangle(0, height / 3 * 2, width, height)
-brushColor('white')
-circle(width / 2, height / 3, 150)
 run()
