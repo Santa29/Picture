72321e74c5c85dee50ab6f3b88cba342922bcfe8
Mikhail Rybolovlev <michaels17525@mail.ru>
2020-03-02 16:07:51 +0300

    Fons done

--- Task #2.py
+++ Task #2.py
@@ -10,8 +10,9 @@ def mushroom(angle, size):
 
 
 def light(x, y, light_width, light_height):
+    """Draw the rectangle by coordinates with parameters width and height"""
     brushColor('yellow')
-    rectangle(x, y, light_width, light_height)
+    rectangle(x, y, x + light_width, y + light_height)
 
 
 width = 1600
@@ -24,4 +25,7 @@ penColor(0, 0, 0)
 brushColor('grey')
 rectangle(0, height / 3 * 2, width, height)
 light(0, 0, width / 15, height / 3 * 2 + 10)
+light(0 + width / 7, 0, width / 6, height - 15)
+light(0.9 * width, 0, width * 0.09, 0.75 * height)
+light(0.75 * width, 0, 0.1 * width, height * 0.85)
 run()
