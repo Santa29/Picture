a202bd8780badca011b52e7b40bb3475e8c6a4cc
Mikhail Rybolovlev <michaels17525@mail.ru>
2020-03-02 15:59:01 +0300

    Add light func

--- Task #2.py
+++ Task #2.py
@@ -5,12 +5,13 @@ def hedgehog(x, y, size):
     pass
 
 
-def light(x, y, size, invert):
-    pass
-
-
 def mushroom(angle, size):
     pass
+
+
+def light(x, y, light_width, light_height):
+    brushColor('yellow')
+    rectangle(x, y, light_width, light_height)
 
 
 width = 1600
@@ -18,8 +19,9 @@ height = 900
 windowSize(width, height)
 penColor(0, 0, 0)
 brushColor(0, 150, 0)
-print(coords(rectangle(0, 0, width, height / 3 * 2)))
+rectangle(0, 0, width, height / 3 * 2)
 penColor(0, 0, 0)
 brushColor('grey')
 rectangle(0, height / 3 * 2, width, height)
+light(0, 0, width / 15, height / 3 * 2 + 10)
 run()
