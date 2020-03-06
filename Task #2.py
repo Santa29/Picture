6240b4285b720cb0fab60b2a64f2c4d832cf592f
Mikhail Rybolovlev <michaels17525@mail.ru>
2020-03-06 13:19:20 +0300

    Partially done mushroom func.

--- Task #2.py
+++ Task #2.py
@@ -8,7 +8,7 @@ def oval_with_angle(x, y, size, angle, color):
     penColor(color)
     point_massive_for_oval = []
     for j in range(361):
-        a = 3 * size * math.cos(j)
+        a = 2 * size * math.cos(j)
         b = size * math.sin(j)
         a, b = a * math.cos(angle) - b * math.sin(angle), -1 * a * math.sin(angle) + b * math.cos(angle)
         k = (a + x, b + y)
@@ -19,7 +19,10 @@ def oval_with_angle(x, y, size, angle, color):
 
 def mushroom(x, y, angle, size):
     """Draw the mushroom. Coordinates is the right and left corners of the rectangle, which is the border of oval"""
-    pass
+    oval_with_angle(x, y - 1.5 * size, size * 0.5, angle, 'white')
+    oval_with_angle(x - 2* size * math.cos(angle), y + 2.5 * size * math.sin(angle), size, angle + 90, 'red')
+    oval_with_angle(x, y + 2.7 * size * math.sin(angle), size * 0.1, angle + 90, 'white')
+    oval_with_angle(x - size, y + 2.2 * size * math.sin(angle), size * 0.1, angle + 90, 'white')
 
 
 def hedgehog(x, y, size):
@@ -45,6 +48,6 @@ light(0, 0, width / 15, height / 3 * 2 + 10)
 light(0 + width / 7, 0, width / 6, height - 15)
 light(0.9 * width, 0, width * 0.09, 0.75 * height)
 light(0.75 * width, 0, 0.1 * width, height * 0.85)
-mushroom(width / 2, height / 2, 30, 15)
-oval_with_angle(width / 2, height / 2, 50, 7, 'red')
+mushroom(width / 2, height / 2, 45, 40)
+oval_with_angle(width / 2, height / 2, 50, math.pi/8, 'blue')
 run()
