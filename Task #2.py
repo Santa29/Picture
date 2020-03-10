79027803a2a813f8d0f4db646e29e623852020b9
Mikhail Rybolovlev <michaels17525@mail.ru>
2020-03-10 17:26:42 +0300

    Add the first plan function.

--- Task #2.py
+++ Task #2.py
@@ -30,7 +30,7 @@ def spots(x, y, angle, size):
         for j in range(6):
             a[j][0], a[j][1] = x + a[j][0] * size, y + a[j][1] * size
             length = math.pow(pow(x - a[j][0], 2) + pow(y - a[j][1], 2), 0.5)
-            start_angle = math.asin((a[j][0] - x)/length)
+            start_angle = math.asin((a[j][0] - x) / length)
             delta_x = length * math.sin(start_angle + math.radians(angle))
             delta_y = length * math.cos(start_angle + math.radians(angle))
             oval_with_angle(x + delta_x, y + delta_y, random.randint(10, 19) / 100 * size, angle, 'white')
@@ -52,6 +52,12 @@ def mushroom(x, y, angle, size):
         oval_with_angle(x + 2 * size * math.sin(math.radians(angle)), y + 1.5 * size, size * 0.6, angle + 90, 'white')
     oval_with_angle(x, y, size, angle + math.pi / 2, 'red')
     spots(x, y, angle, size)
+
+
+def first_plan(max_width, width_mushrooms, height_max, size):
+    """Draw the line of mushrooms on the first plane"""
+    for j in range(5):
+        mushroom((max_width - width_mushrooms) / 5 * j + width_mushrooms, height_max, random.randint(-15, 15), size)
 
 
 def hedgehog(x, y, size):
@@ -78,4 +84,5 @@ light(0 + width / 7, 0, width / 6, height - 15)
 light(0.9 * width, 0, width * 0.09, 0.75 * height)
 light(0.75 * width, 0, 0.1 * width, height * 0.85)
 mushroom(width / 2, height / 2, -15, 20)
+first_plan(width, width / 2, height * 0.8, 30)
 run()
