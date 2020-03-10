dc618c6a6fc6630b786835c6475837d52894fa0b
Mikhail Rybolovlev <michaels17525@mail.ru>
2020-03-10 17:10:59 +0300

    Points function work good now.

--- Task #2.py
+++ Task #2.py
@@ -40,7 +40,7 @@ def spots(x, y, angle, size):
             length = math.pow(pow(x - a[j][0], 2) + pow(y - a[j][1], 2), 0.5)
             start_angle = math.asin((a[j][0] - x) / length)
             delta_x = length * math.sin(start_angle - math.radians(angle))
-            delta_y = length * math.cos(start_angle - math.radians(angle))
+            delta_y = length * math.cos(start_angle + math.radians(angle))
             oval_with_angle(x + delta_x, y + delta_y, random.randint(10, 19) / 100 * size, angle, 'white')
 
 
