8574e669ee1f75b32a503ce0bf3f6e70b1701ae5
Mikhail Rybolovlev <michaels17525@mail.ru>
2020-03-13 14:25:34 +0300

    Add the spine function.

--- Task #2.py
+++ Task #2.py
@@ -61,6 +61,15 @@ def first_plan(max_width, width_mushrooms, height_max):
         mushroom(x_coord, height_max, random.randint(-15, 15), random.randint(15, 50))
 
 
+def spine(x, y, angle, size):
+    penColor('black')
+    brushColor('black')
+    x1 = (x + size * math.sin(angle), y - size * math.cos(angle))
+    x2 = (x + 0.1* size * math.cos(angle), y + math.sin(angle))
+    points = [(x, y), x1, x2]
+    polygon(points)
+
+
 def hedgehog(x, y, size):
     pass
 
@@ -84,6 +93,5 @@ light(0, 0, width / 15, height / 3 * 2 + 10)
 light(0 + width / 7, 0, width / 6, height - 15)
 light(0.9 * width, 0, width * 0.09, 0.75 * height)
 light(0.75 * width, 0, 0.1 * width, height * 0.85)
-mushroom(width / 2, height / 2, -15, 20)
 first_plan(width, width / 2, height * 0.93)
 run()
